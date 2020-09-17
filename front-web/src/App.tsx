0eedb479a141955483e29d2370a392bfaba78ad9
felrespin <resendepinto@gmail.com>
2020-09-17 19:50:10 -0300

    Create Header component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+
 
 function App() {
   return (
-    <h1>DEV SUPERIOR</h1>
-
+    
+    <Header />
+    
   );
 }
 
