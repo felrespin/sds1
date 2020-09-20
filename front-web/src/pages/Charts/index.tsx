2b7b1264d21e25382bac608d677b6c3ff839ce96
felrespin <resendepinto@gmail.com>
2020-09-20 16:07:24 -0300

    Fix BASE_URL

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://sds1-felrespin.herokuapp.com';
+const BASE_URL = 'https://sds1-felrespin.herokuapp.com';
 
 const Charts = () => {
 
